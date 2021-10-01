98c56e7cfad317092a99c037899d65f25dc5a462
LuisGuilherme26 <43975548+LuisGuilherme26@users.noreply.github.com>
2021-10-01 13:04:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom"
+const root = document.getElementById('root')
+
+ReactDOM.render(
+    <ul>
+        <li>Luis Guilherme Viana Fernandes</li>
+        <li>19 anos</li>
+        <li>Design Digital</li>
+        <li>Canindé-CE</li>
+        <li>blcknr27@gmail.com</li>
+    </ul>
+    , root)
